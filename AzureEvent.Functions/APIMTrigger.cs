d0488b102e14ff058866e01bc6ed193c4000b649
Karthikeyan tc <karthikeyan.tc@toolagen.com>
2024-05-28 11:58:38 +0530

    Changed the Http trigger authentication

--- AzureEvent.Functions/APIMTrigger.cs
+++ AzureEvent.Functions/APIMTrigger.cs
@@ -17,7 +17,7 @@ namespace AzureEvent.Function
     {
         [FunctionName("APIMTrigger")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
