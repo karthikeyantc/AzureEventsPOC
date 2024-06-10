ccd37ab8a7fd1df422782a37a7658c4f2da176a5
Karthikeyan tc <karthikeyan.tc@toolagen.com>
2024-06-10 22:38:02 +0530

    Function App code optimizations

--- AzureEvent.Functions/EventModel.cs
+++ AzureEvent.Functions/EventModel.cs
@@ -2,19 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
 
 namespace AzureEvent.Functions
 {
+    [JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy))]
     public class EventModel
     {
-        public string topic { get; set; }
-        public string id { get; set; } = Guid.NewGuid().ToString();
-        public string subject { get; set; } = "EventGridModel";
-        public string eventType { get; set; } = "Azure.Sdk.Sample";
-        public object data { get; set; }
-        public string dataVersion { get; set; } = "1.0";
-        public DateTimeOffset eventTime { get; set; } = DateTimeOffset.UtcNow;
+        [JsonProperty(Required = Required.Always)]
+        public string Topic { get; set; }
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Subject { get; set; } = "EventGridModel";
+        public string EventType { get; set; } = "Azure.Sdk.Sample";
+        public object Data { get; set; }
+        public string DataVersion { get; set; } = "1.0";
+        public DateTimeOffset EventTime { get; set; } = DateTimeOffset.UtcNow;
 
     }
 }
--- AzureEvent.Functions/events.cs
+++ AzureEvent.Functions/events.cs
@@ -13,6 +13,7 @@ using Azure;
 using System.Linq;
 using AzureEvent.Functions;
 using Azure.Identity;
+using Newtonsoft.Json.Serialization;
 
 namespace AzureEvent.Function
 {
@@ -23,35 +24,88 @@ namespace AzureEvent.Function
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = "events/{domainName}")] HttpRequest req, string domainName,
             ILogger log)
         {
+            // try
+            // {
+            //     log.LogInformation("The event mapper function is processing a request.");
+
+            //     //Event Grid Domain client
+            //     string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            //     List<EventModel> eventslist = new List<EventModel>();
+            //     var settings = new JsonSerializerSettings();
+            //     settings.Converters.Add(new EventModelConverter());
+            //     settings.ContractResolver = new DefaultContractResolver
+            //     {
+            //         NamingStrategy = new CamelCaseNamingStrategy()
+            //     };
+            //     // if (requestBody.TrimStart().StartsWith("["))
+            //     // {
+            //     //     eventslist = JsonConvert.DeserializeObject<List<EventModel>>(requestBody, settings);
+            //     //     // Process the list
+            //     // }
+            //     // else
+            //     // {
+            //     //     eventslist = new List<EventModel> { JsonConvert.DeserializeObject<EventModel>(requestBody, settings) };
+            //     //     // Process the single object
+            //     // }
+            //     eventslist = JsonConvert.DeserializeObject<List<EventModel>>(requestBody, settings);
+            //     string domainEndpoint = Environment.GetEnvironmentVariable($"DomainEndpoint{domainName.ToLower()}");
+            //     // string domainKey = Environment.GetEnvironmentVariable($"DomainKey{domainName.ToLower()}");
+            //     // EventGridPublisherClient client = new EventGridPublisherClient(new Uri(domainEndpoint), new AzureKeyCredential(domainKey));
+            //     var clientId = "145a79a6-042d-4be2-9f56-9caf813c3a53"; // Replace with your Managed Identity's Client Id
+            //     var options = new DefaultAzureCredentialOptions { ManagedIdentityClientId = clientId };
+            //     var credential = new DefaultAzureCredential(options);
+            //     EventGridPublisherClient client = new EventGridPublisherClient(new Uri(domainEndpoint), credential);
+            //     var settings1 = new JsonSerializerSettings
+            //     {
+            //         ContractResolver = new DefaultContractResolver
+            //         {
+            //             NamingStrategy = new CamelCaseNamingStrategy()
+            //         }
+            //     };
+            //     string json = JsonConvert.SerializeObject(eventslist, settings1);
+            //     log.LogInformation($"Events: {json}");
+            //     BinaryData requestBodyBinary = BinaryData.FromString(json);
+            //     List<EventGridEvent> events = EventGridEvent.ParseMany(requestBodyBinary).ToList();
+            //     Response result = await client.SendEventsAsync(events);
+            //     // return the result's response content as a string
+            //     if (result.Status == 200)
+            //         return new OkObjectResult("Events published successfully");
+            //     else
+            //         return new BadRequestObjectResult(result.ContentStream);
+            // }
+            // catch (Exception ex)
+            // {
+            //     log.LogError($"An error occurred: {ex.Message}");
+            //     return new BadRequestObjectResult("An error occurred with message: " + ex.Message);
+            // }
             try
             {
                 log.LogInformation("The event mapper function is processing a request.");
 
                 //Event Grid Domain client
-                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
                 List<EventModel> eventslist = new List<EventModel>();
                 var settings = new JsonSerializerSettings();
                 settings.Converters.Add(new EventModelConverter());
-                if (requestBody.TrimStart().StartsWith("["))
+
+                using (var streamReader = new StreamReader(req.Body))
+                using (var jsonReader = new JsonTextReader(streamReader))
                 {
-                    eventslist = JsonConvert.DeserializeObject<List<EventModel>>(requestBody, settings);
-                    // Process the list
+                    var serializer = JsonSerializer.Create(settings);
+                    eventslist = serializer.Deserialize<List<EventModel>>(jsonReader);
                 }
-                else
-                {
-                    eventslist = new List<EventModel> { JsonConvert.DeserializeObject<EventModel>(requestBody, settings) };
-                    // Process the single object
-                }
+
                 string domainEndpoint = Environment.GetEnvironmentVariable($"DomainEndpoint{domainName.ToLower()}");
-                // string domainKey = Environment.GetEnvironmentVariable($"DomainKey{domainName.ToLower()}");
-                // EventGridPublisherClient client = new EventGridPublisherClient(new Uri(domainEndpoint), new AzureKeyCredential(domainKey));
                 var clientId = "145a79a6-042d-4be2-9f56-9caf813c3a53"; // Replace with your Managed Identity's Client Id
                 var options = new DefaultAzureCredentialOptions { ManagedIdentityClientId = clientId };
                 var credential = new DefaultAzureCredential(options);
                 EventGridPublisherClient client = new EventGridPublisherClient(new Uri(domainEndpoint), credential);
-                BinaryData requestBodyBinary = BinaryData.FromString(JsonConvert.SerializeObject(eventslist));
+
+                string json = JsonConvert.SerializeObject(eventslist);
+                log.LogInformation($"Events: {json}");
+                BinaryData requestBodyBinary = BinaryData.FromString(json);
                 List<EventGridEvent> events = EventGridEvent.ParseMany(requestBodyBinary).ToList();
                 Response result = await client.SendEventsAsync(events);
+
                 // return the result's response content as a string
                 if (result.Status == 200)
                     return new OkObjectResult("Events published successfully");
@@ -73,11 +127,11 @@ namespace AzureEvent.Function
             serializer.Populate(reader, eventModel);
 
             // Ensure default values
-            if (string.IsNullOrWhiteSpace(eventModel.id)) eventModel.id = Guid.NewGuid().ToString();
-            if (string.IsNullOrWhiteSpace(eventModel.subject)) eventModel.subject = "EventGridModel";
-            if (string.IsNullOrWhiteSpace(eventModel.eventType)) eventModel.eventType = "Azure.Sdk.Sample";
-            if (eventModel.eventTime == default) eventModel.eventTime = DateTimeOffset.UtcNow;
-            if (string.IsNullOrWhiteSpace(eventModel.dataVersion)) eventModel.dataVersion = "1.0";
+            if (string.IsNullOrWhiteSpace(eventModel.Id)) eventModel.Id = Guid.NewGuid().ToString();
+            if (string.IsNullOrWhiteSpace(eventModel.Subject)) eventModel.Subject = "EventGridModel";
+            if (string.IsNullOrWhiteSpace(eventModel.EventType)) eventModel.EventType = "Azure.Sdk.Sample";
+            if (eventModel.EventTime == default) eventModel.EventTime = DateTimeOffset.UtcNow;
+            if (string.IsNullOrWhiteSpace(eventModel.DataVersion)) eventModel.DataVersion = "1.0";
 
             return eventModel;
         }
